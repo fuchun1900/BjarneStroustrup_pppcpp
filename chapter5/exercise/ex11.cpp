137c66f23d9ec0d350040a4570c3d606a15cf5b7
fuchun1900 <60424580+fuchun1900@users.noreply.github.com>
2021-11-30 22:36:49 +0800

    Add files via upload

--- chapter5/exercise/ex11.cpp
+++ chapter5/exercise/ex11.cpp
@@ -3,9 +3,9 @@ int main()
 {
     try
     {
-        constexpr int biggest_int = 100000;
-        int a =0 ,b = 1,fibo;
-        while (fibo <= biggest_int)
+        //constexpr int biggest_int = 100000; //当输入int最大数时注意因为超过int容量而产生无限循环
+        int a =0 ,b = 1,fibo = 1;
+        while (fibo >= a)  //当int溢出后为负数，相加后必定比最后一个数小，所以输出最后一个数
         {
             fibo = a + b;
             a = b;
